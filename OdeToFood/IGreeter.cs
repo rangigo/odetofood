835fa1b5a1424dd7af95c96cb7801eb2479a223e
rangigo <phuoc.nguyen@nordpoolgroup.com>
2019-01-18 18:02:42 +0200

    Getting used to .NET Core configuration

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,24 @@
+﻿namespace OdeToFood
+{
+    using Microsoft.Extensions.Configuration;
+
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private readonly IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+    
+        public string GetMessageOfTheDay()
+        { 
+            return _configuration["Greeting"];
+        }
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -9,26 +9,31 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace OdeToFood
 {
+    using Microsoft.Extensions.Configuration;
+
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = greeter.GetMessageOfTheDay();
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
+ 
 }
