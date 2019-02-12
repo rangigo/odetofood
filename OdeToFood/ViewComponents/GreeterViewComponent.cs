e4f6c8d3a99bb33a09616364a5bf84b1bfbbed0b
rangigo <phuoc.nguyen@nordpoolgroup.com>
2019-02-12 15:38:48 +0200

    Practice implementing View Component

---
+++ OdeToFood/ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OdeToFood.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetMessageOfTheDay();
+            return View("Default", model);
+        }
+    }
+}
