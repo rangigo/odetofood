dbdfa3580791264f43252680528f297e5eec76aa
rangigo <phuoc.nguyen@nordpoolgroup.com>
2019-02-12 14:02:00 +0200

    Add SQL database and utilize .NET Entity Framework to map entities to database

--- OdeToFood/Models/Restaurant.cs
+++ OdeToFood/Models/Restaurant.cs
@@ -11,6 +11,7 @@ namespace OdeToFood.Models
         public int Id { get; set; }
 
         [Display(Name = "Restaurant Name")]
+        [Required, MaxLength(80)]
         public string Name { get; set; }
 
         public CuisineType Cuisine { get; set; }
